cd6be8a1cbf66329e06815150e4eb29e8dac1eec
EC2 Default User <gh01.yuta@gmail.com>
2020-10-06 13:44:27 +0000

    [task5-f]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get "home/about" => "about#index"
   devise_for :users
   resources :users, only: [:show, :edit, :update, :index ] do
-    # get :search, on: :collection
     member do
       get :following, :followers
     end
